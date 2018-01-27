f9f2980e10053525d1a64fd1d2a94c1c7a649df6
RobertoLeogrande <boris47@hotmail.it>
2018-01-27 01:24:39 +0100

    Cammera follow ( no rotation )

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+	[ SerializeField ]
+	private	Transform		m_Target					= null;
+
+	[ SerializeField ]
+	private	Vector3			m_Offset					= Vector3.zero;
+
+	Vector3 startRotation = Vector3.zero;
+
+	private void Start()
+	{
+		startRotation = ( m_Target.position - transform.position );
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Vector3.Lerp( transform.position, m_Target.transform.position, Time.deltaTime * 8f );
+		transform.position += transform.TransformDirection( m_Offset );
+
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public	float	speed = 1f;
+	public	float	traslation = 50f;
+	public	float	accelleration = 1f;
+	public	float	decelleration = 1f;
+
+	private	float	lerpedTraslation = 0f;
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float currentTraslation = 0f;
+		if ( Input.GetKey( KeyCode.A ) )
+		{
+			currentTraslation = traslation;
+		}
+
+		if ( Input.GetKey( KeyCode.D ) )
+		{
+			currentTraslation = -traslation;
+		}
+
+
+		lerpedTraslation = Mathf.Lerp
+		( 
+			lerpedTraslation, 
+			currentTraslation,  
+			( currentTraslation != 0f ? accelleration : decelleration ) * Time.deltaTime
+		);
+
+
+		RaycastHit hit;
+		if( Physics.Raycast( transform.position, -transform.up, out hit ) == false )
+		{
+			print( "Porca madonna" );
+			return;
+		}
+
+		// rotate around cylinder center
+		Vector3 centerPoint = hit.point - ( hit.normal * 1.5f );
+		transform.RotateAround( centerPoint, transform.forward, lerpedTraslation * Time.deltaTime );
+
+		// Move forward
+		transform.position += transform.forward * ( Time.deltaTime * speed );
+
+	}
+}
