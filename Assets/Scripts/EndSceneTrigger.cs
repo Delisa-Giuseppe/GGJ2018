e6c684317b4a63d0b39cd479139c547e37c9decf
RobertoLeogrande <boris47@hotmail.it>
2018-01-27 21:58:06 +0100

    Fader di fine livello creato

---
+++ Assets/Scripts/EndSceneTrigger.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneTrigger : MonoBehaviour {
+
+	[ SerializeField ]
+	private	Player	m_Player	= null;
+
+	[ SerializeField ]
+	private	string	m_SceneName	= "";
+
+	// Use this for initialization
+	void Start()
+	{
+		DontDestroyOnLoad( this );
+	}
+
+	// Async load of the next scene
+	private	IEnumerator	SceneLoad()
+	{
+
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync( m_SceneName );
+		asyncLoad.allowSceneActivation = true;
+		
+		//Wait until the last operation fully loads to return anything
+		while ( !asyncLoad.isDone )
+		{
+			yield return null;
+		}
+
+		Player player = FindObjectOfType<Player>();
+		float startSpeed = player.Speed;
+		player.Speed = 0f;
+		Fader.Instance.FadeIn( () => { player.Speed = startSpeed; } );
+	}
+
+
+	private void OnTriggerEnter( Collider other )
+	{
+		if ( other.tag != "Player" )
+			return;
+
+		if ( m_Player == null )
+		{
+			print( "IL PLAYER DEVI INSERIRE, PIRLA!!!" );
+			return;
+		}
+		m_Player.Speed = 0f;
+
+		Fader.Instance.FadeOut( () => StartCoroutine( SceneLoad() ) );
+
+		Destroy( GetComponent<Collider>() );	
+	}
+
+
+
+}
---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fader : MonoBehaviour {
+
+
+	public	static Fader Instance			= null;
+
+	public Image		m_FadeImage			= null;
+	public	float		m_TransitionTime	= 2f;
+
+
+
+	private	float		Interpolant			= 0f;
+	
+	private	bool		m_InOverride		= false;
+	private	bool		m_FadeInInternal	= false;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		if ( Instance != null )
+		{
+			Destroy( gameObject );
+			return;
+		}
+		Instance = this;
+		DontDestroyOnLoad( this );
+	}
+
+
+	private IEnumerator Fade( bool fadeIn, System.Action callback = null )
+	{
+		Interpolant = 0f;
+		float	currentTime = 0f;
+		Color	startColor = m_FadeImage.color;
+		while( Interpolant < 1f )
+		{
+			currentTime += Time.unscaledDeltaTime;
+			Interpolant = currentTime / m_TransitionTime;
+			float a = ( fadeIn == true ) ? m_FadeImage.color.a - Interpolant : m_FadeImage.color.a + Interpolant;
+			m_FadeImage.color = new Color ( 0, 0, 0, a );
+			yield return null;
+		}
+		Interpolant = 0f;
+
+		print( fadeIn ? "Fadein:":"FadeOut" );
+
+		if ( callback != null )
+			callback();
+	}
+
+
+	public	void	FadeIn( System.Action callback = null )
+	{
+		StopAllCoroutines();
+		StartCoroutine( Fade( fadeIn: true, callback: callback ) );
+	}
+
+
+	public	void	FadeOut( System.Action callback = null )
+	{
+		StopAllCoroutines();
+		StartCoroutine( Fade( fadeIn: false, callback: callback ) );
+	}
+}
